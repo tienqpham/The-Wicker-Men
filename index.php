67779a567c69d41807dc62284bc24c22c5228be3
tienqpham97@gmail.com <tienqpham97@gmail.com>
2018-11-27 23:34:44 -0600

    added login page and home page

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
 <!DOCTYPE html>
 <html>
 <body>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+background:url(images/nick.jpg);
+  margin: 0;
+  font-family:Verdana, Geneva, sans-serif;
+}
 
-<h1>My first PHP page</h1>
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
 
-<?php
-echo "Hello World!";
-?>
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="temp.php">Actor List</a>
+  <a href="temp.php">Movie List</a>
+  <a href="temp.php">Studio List</a>
+  <a href="login.php">Logout</a>
+</div>
+
+<div style="padding-left:16px">
+  <h2>Welome to The Wicker Men Database</h2>
+  <p>Nicolas Cage</p>
+  <p>Nicolas Cage</p>
+  <p>Nicolas Cage</p>
+  <p>Nicolas Cage</p>
+</div>
 
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>The Wicker Men</title>
+<style type="text/css">
+body{
+	background:#000000;
+	font-size:17px;
+	font-family:Verdana, Geneva, sans-serif;
+}
+
+table{
+	background:#FFF;
+	margin-top:15%;
+	border:#666 1px solid;
+	border-radius:5px;
+	padding:0px;
+}
+
+th{
+	background:#FFF;
+}
+
+td{
+	padding:10px;
+}
+
+input[type=text], input[type=password] {
+    border-left:5px #FF900 solid;
+}
+
+input[type=submit]{
+	border-radius:2px;
+	height:25px;
+	border:none;
+}
+
+</style>
+</head>
+
+<body>
+<?php
+if(isset($_POST['btn_submit'])){
+	
+	$username = $_POST['user'];
+	$password = $_POST['pass'];
+		
+	if($username == "admin" && $password == "admin"){
+		header('location: index.php');
+	}
+	else{
+		header("location:login.php?status=false");
+	}
+}
+?>
+<form action="" method="post">
+<table align="center">
+	<tr>
+		<th colspan="2">Login</th>
+	</tr>
+	<?php
+		if(isset($_GET['status']) && $_GET['status'] == "false"){
+	?>
+	<tr>
+		<td colspan="2" align="center"><font size="2" color="#FF0000">Wrong Username or Password!</font></td>
+	</tr>
+	<?php
+	}
+	?>
+	<tr>
+		<td>Username :</td><td><input type="text" name="user" /></td>
+	</tr>
+	<tr>
+		<td>Password :</td><td><input type="password" name="pass" /></td>
+	</tr>
+	<tr>
+		<td></td><td><input type="submit" name="btn_submit" value="Login"/></td>
+	</tr>
+</table>
+</form>
+</body>
+</html>
