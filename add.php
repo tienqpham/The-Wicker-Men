80c36a8f83ee7666d9e3f9b6dab302bbbe720160
Harrison Connor Malloy <hmalloy@lab10.cs.ndsu.nodak.edu>
2018-12-05 20:33:27 -0600

    IP safe and flexible insertion interface

--- add.php
+++ add.php
@@ -27,6 +27,39 @@ if($_SESSION['status']!="Active"){
     header("location:login.php");
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["first"])) 
+	{
+		$emptyErr = "Field is required";
+	} 
+	else
+    {
+       $first = test_input($_POST["first"]);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["second"])) 
+	{
+		$emptyErr = "Field is required";
+	} 
+	else
+    {
+       $second = test_input($_POST["second"]);
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (empty($_POST["third"])) 
+	{
+		$emptyErr = "Field is required";
+	} 
+	else
+    {
+       $third = test_input($_POST["third"]);
+    }
+}
+
 ?>
 <html>
 <head>
@@ -70,32 +103,158 @@ body {
   <a href="actorlist.php">Actor List</a>
   <a href="movielist.php">Movie List</a>
   <a href="tvseries.php">Tv Series</a>
-  <a href="temp.php">Add Data</a>
+  <a href="add.php">Add Data</a>
   <a href="search.php">Search</a>
   <a href="logout.php">Logout</a>
 </div>
-<select>
+<form action="" method="post">
+<select  name="tableSelect" id="tableSelect">
+	<option value="">None Selected</option>
 	<option value="people">People</option>
 	<option value="films">Films</option>
-	<option value="tv">TV Series</option>
-	<option value="stage">Stage Play</option>
+	<option value="TVSeries">TV Series</option>
+	<option value="stagePlays">Stage Play</option>
 	<option value="pseudonyms">Pseudonyms</option>
 	<option value="filmStaff">Film Staff</option>
-	<option value="tvStaff">TV Staff</option>
-	<option value="stageStaff">Stage Play Staff</option>
+	<option value="TVStaff">TV Staff</option>
+	<option value="stagePlayStaff">Stage Play Staff</option>
 </select>
-<form>
-  First Entry (ID):<br>
-  <input type="text" name="firstentry"><br>
-  Second Entry:<br>
-  <input type="text" name="secondentry"><br>
-  Third Entry:<br>
-  <input type="text" name="thirdentry"><br>
-  Fourth Entry:<br>
-  <input type="text" name="fourthentry"><br>
-  Fifth Entry:<br>
-  <input type="text" name="fifthentry"><br>
-  <input type="submit" value="Submit">
-</form> 
+<button onclick="<?php?>">choose table</button>
+</form>
+<?php
+$query = '';
+if (isset($_POST['tableSelect']))
+{
+	$table = $_POST['tableSelect'];
+	
+	
+	
+	echo $table;
+	
+	?>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	<?php
+	if($table == "people")
+	{
+		?>
+			pID (unique identifier)<br>
+			<input type="text" name="first">
+			<span class="error">* <?php echo $emptyErr;?> </span><br>
+			First Name<br>
+			<input type="text" name="second">
+			<span class="error">* <?php echo $emptyErr;?> </span><br>
+			Last Name<br>
+			<input type="text" name="third">
+			<span class="error">* <?php echo $emptyErr;?> </span><br>
+			Birth Date (can be left empty if unknown)<br>
+			<input type="text" name="fourth"><br>
+			Death Date (can be left empty if alive)<br>
+			<input type="text" name="fifth"><br>
+			
+			<input type="submit" name="submit" value="button">
+		</form>
+		<?php
+		$birthDate = $fourth;
+		
+		$deathDate = $fifth;
+		
+		if($fourth == '')
+			$birthDate = 'NULL';
+			
+		if($fifth == '')
+			$deathDate = 'NULL';
+		
+		$query = 'INSERT INTO people VALUES('.$first.','.$second.','.$third.','.$birthDate.','.$deathDate.')' ;
+		
+		//echo $query;
+	}
+	else if($table == "films")
+	{
+		?>
+			fID (unique identifier)<br>
+			<input type="text" name="fIDBox"><br>
+			Name<br>
+			<input type="text" name="fNameBox"><br>
+			Release Date (can be left empty if unknown)<br>
+			<input type="text" name="fDateBox"><br>
+			Budget (can be left empty if unknown)<br>
+			<input type="text" name="fBudgetBox"><br>
+		<?php
+	}
+	else if($table == "TVSeries")
+	{
+		?>
+			tID (unique identifier)<br>
+			<input type="text" name="tIDBox"><br>
+			Name<br>
+			<input type="text" name="tNameBox"><br>
+			Episode Count<br>
+			<input type="text" name="tEpBox"><br>
+			Release Date (can be left empty if unknown)<br>
+			<input type="text" name="tDateBox"><br>
+			Budget (can be left empty if unknown)<br>
+			<input type="text" name="tBudgetBox"><br>
+		<?php
+	}
+	else if($table == "stagePlays")
+	{
+		?>
+			sID (unique identifier)<br>
+			<input type="text" name="sIDBox"><br>
+			Name <br>
+			<input type="text" name="sNameBox"><br>
+			Release Date (can be left empty if unknown)<br>
+			<input type="text" name="sDateBox"><br>
+			Budget (can be left empty if unknown)<br>
+			<input type="text" name="sBudgetBox"><br>
+		<?php
+	}
+	else if($table == "pseudonyms")
+	{
+		?>
+			pID of a recorded person<br>
+			<input type="text" name="pIDBox"><br>
+			Pseudonym <br>
+			<input type="text" name="pseudonymBox"><br>
+		<?php
+	}
+	else if($table == "filmStaff")
+	{
+		?>
+			pID of a recorded person<br>
+			<input type="text" name="pIDBox"><br>
+			fID of a recorded film<br>
+			<input type="text" name="fIDBox"><br>
+			Position (e.g. Director) or Role (e.g. "Nicolas")<br>
+			<input type="text" name="roleBox"><br>
+		<?php
+	}
+	else if($table == "TVStaff")
+	{
+		?>
+			pID of a recorded person<br>
+			<input type="text" name="pIDBox"><br>
+			tID of a recorded TV series<br>
+			<input type="text" name="tIDBox"><br>
+			Position (e.g. Director) or Role (e.g. "Nicolas")<br>
+			<input type="text" name="roleBox"><br>
+		<?php
+	}
+	else if($table == "stagePlayStaff")
+	{
+		?>
+			pID of a recorded person<br>
+			<input type="text" name="pIDBox"><br>
+			sID of a recorded stage play<br>
+			<input type="text" name="sIDBox"><br>
+			Position (e.g. Director) or Role (e.g. "Nicolas")<br>
+			<input type="text" name="roleBox"><br>
+		<?php
+	}
+	
+	
+}
+echo $query;
+?>
 </body>
 </html>
